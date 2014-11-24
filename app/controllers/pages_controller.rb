5032783ed84c2b55c169c7bf12d6f4bb39d4edd6
Tigran Terian <tigran.terian@gmail.com>
2014-11-24 17:40:59 +0400

    third commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,17 @@ class PagesController < ApplicationController
   end
 
   def search
-  	@query = params[:q]
+    @query = params[:q]
+
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "TPiF7v0F3cAEC9KpSgrmMEyp7"
+      config.consumer_secret     = "VTh9vs3b8QcBbSn0BgddbMKGraUpD4Ky50IM896woDrqi66bNH"
+      config.access_token        = "2457919124-gInYE2ljgGB5tYyisA1rwkxRO612Q17wiDFVPcb"
+      config.access_token_secret = "twm9pmbseJkY4ZYvdNlEWWSqi48zk7kqwnyF4cKfyVIgk"
+    end
+  	 
+    @result = client.user_search("#{@query}", count: 5)
+#    @profile = client.profile("#{@query}")
   end
 
   def about
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,24 +1,16 @@
 class Search < ActiveRecord::Base
 
-  def initialize
-    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  attr_accessor :client
 
-    @client = Twitter::REST::Client.new do |config|
+  def self.search(user)
+
+      @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "TPiF7v0F3cAEC9KpSgrmMEyp7"
       config.consumer_secret     = "VTh9vs3b8QcBbSn0BgddbMKGraUpD4Ky50IM896woDrqi66bNH"
       config.access_token        = "2457919124-gInYE2ljgGB5tYyisA1rwkxRO612Q17wiDFVPcb"
       config.access_token_secret = "twm9pmbseJkY4ZYvdNlEWWSqi48zk7kqwnyF4cKfyVIgk"
     end
-  end
 
-  def update(status)
-      Search.initialize()
-      client.update(#{@status})
-  end
-
-  def search
-    Search.initialize(user)
-    client.user("@{#user}")
   end
 
 end
